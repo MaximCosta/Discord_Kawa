d75a72299061f11ad5905ce9b7e3eea6aae8c2ea
nathan <Nathan>
2022-03-02 00:41:34 +0100

    [ADD] actually test

--- bot.js
+++ bot.js
@@ -437,6 +437,61 @@ client.on("messageCreate", async message => {
             }
         }
     }
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content
+      .toLowerCase()
+      .slice(prefix.length)
+      .trim()
+      .split(/\s+/);
+    const [command, input] = args;
+  
+    if (command === 'clear' || command === 'c') {
+      if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+        return message.channel
+          .send(
+            "You cant use this command since you're missing `manage_messages` perm",
+          );
+      }
+  
+      if (isNaN(input)) {
+        return message.channel
+          .send('enter the amount of messages that you would like to clear')
+          .then((sent) => {
+            setTimeout(() => {
+              sent.delete();
+            }, 2500);
+          });
+      }
+  
+      if (Number(input) < 0) {
+        return message.channel
+          .send('enter a positive number')
+          .then((sent) => {
+            setTimeout(() => {
+              sent.delete();
+            }, 2500);
+          });
+      }
+  
+      // add an extra to delete the current message too
+      const amount = Number(input) > 100
+        ? 101
+        : Number(input) + 1;
+  
+      message.channel.bulkDelete(amount, true)
+      .then((_message) => {
+        message.channel
+          // do you want to include the current message here?
+          // if not it should be ${_message.size - 1}
+          .send(`Bot cleared \`${_message.size}\` messages :broom:`)
+          .then((sent) => {
+            setTimeout(() => {
+              sent.delete();
+            }, 2500);
+          });
+      });
+    }
 });
 
 client.on('guildMemberAdd', guildMember => {
