82a89d0663d7c92458fd837a45de91fe7c61d8a3
nathan <Nathan>
2022-03-02 01:00:00 +0100

    [ADD] actually test

--- bot.js
+++ bot.js
@@ -453,15 +453,12 @@ client.on("messageCreate", async message => {
 });
 
 client.on('guildMemberAdd', guildMember => {
-    timer = timer + 1;
-    if (timer % 2 == 0) {
-        const embed_welcome = new MessageEmbed()
+    const embed_welcome = new MessageEmbed()
         .setColor("#FF9900")
         .setFooter("Coded by MisTrou")
         .setDescription("Bienvenue à <@" + guildMember.id + ">")
         .setThumbnail("https://cdn.discordapp.com/attachments/947974277691473920/948245206866735124/logo_kawa.png");
-        client.channels.cache.get("948358220123090994").send({ embeds: [embed_welcome] });
-    }
+    guildMember.guild.channels.cache.get("948358220123090994").send({ embeds: [embed_welcome] });
 });
 
 client.login(process.env.BOT_TOKEN);
